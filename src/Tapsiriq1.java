8941da1d61609b6326de0df2e7f4ec332bec026d
fatimaqara999 <fatimaqara3@gmail.com>
2026-07-10 12:58:37 +0400

    Initial commit

---
+++ src/Tapsiriq1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tapsiriq9 {
+     public static void main(String[] argsr) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Bali daxil edin");
+    int bal=scanner.nextInt();
+    String qiymet;
+    if ( bal >= 90) {
+        qiymet = "ela";
+    } else if (bal >=70 ) {
+        qiymet = "yaxsi";
+    } else if (bal >=50) {
+        qiymet = "kafi";
+    } else {
+        qiymet = "Qeyri-kafi";
+    }
+    System.out.println(" Bal: " +bal+  " Qiymet: " + qiymet );
+         }
+
+     }
+
